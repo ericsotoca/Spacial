753ef6302d08fe67ee7447b88b94de6c7122463d
Eric Sotoca <151804279+ericsotoca@users.noreply.github.com>
2026-07-08 22:32:16 +0200

    chore: update base path for deployment
    Update the Vite base configuration to /Spacial/ to ensure correct asset resolution when deployed.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Spacial/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
